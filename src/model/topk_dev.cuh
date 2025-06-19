7a67a81f8712d862f4ac0d7abfd50a1d051f2229
RouteTrace <1092841848@qq.com>
2025-06-19 19:48:02 +0800

    fix top_size greater than 32 or top_size equal to 64, avoiding extra kernel launching cost.

--- src/model/topk_dev.cuh
+++ src/model/topk_dev.cuh
@@ -3,17 +3,18 @@
 #include "../trait.cuh"
 #include <iostream>
 /* 面向4090GPU的开发版本 
-
+    不一定兼容top>64的情况
 */
 namespace functions {
 namespace {
-template<typename T, int N>
+template<typename T, int TOP_SIZE>
 static __device__ inline void warpBitonicSort(T& v1, int& pos, bool asc) {  // asc:false->最终结果是每个warp中的元素是降序的
     /* N不能等于64，否则j步长为32，不能跨warp交换数据 */
-    int lane_id = threadIdx.x & (N - 1); // equal to threadIdx.x % N 
+    int K = TOP_SIZE < 32 ? TOP_SIZE : 32;
+    int lane_id = threadIdx.x & (TOP_SIZE - 1); // equal to threadIdx.x % N 
     // 外层循环控制k定义要合并的长度,内层循环j决定步长
     #pragma unroll
-    for (int k = 2; k <= N; k *= 2) {
+    for (int k = 2; k <= K; k *= 2) {
         bool desc = ((lane_id & k) == 0) ^ asc; //以k为范围，交替升/降序: desc
         #pragma unroll
         for (int j = k / 2; j > 0; j /= 2) {
@@ -27,13 +28,15 @@ static __device__ inline void warpBitonicSort(T& v1, int& pos, bool asc) {  // a
         }
     }
 }
-template<typename T, int N>
+template<typename T, int TOP_SIZE>
 static __device__ inline void warpBitonicMerge(T& v1, int& pos1, T& v2, int& pos2) {//比较两个warp之间的伙伴线程，然后进行归并。结果是每两个伙伴warp选出top warp_size个元素，放到第一个warp中
     if (v1 < v2 || (v1 == v2 && pos1 > pos2)) {
         v1 = v2;
         pos1 = pos2;
     }
+    int N = TOP_SIZE < 32 ? TOP_SIZE : 32;
     int lane_id = threadIdx.x & (N - 1);
+    
     // resort
     #pragma unroll
     for (int j = N / 2; j > 0; j /= 2) {
@@ -64,14 +67,17 @@ static __device__ inline void blockBitonicReduce(T& v, int& pos) {
                 int idx_next = (i << 1) - threadIdx.x - 1; // 伙伴线程id,eg. 1023 = 1024 - 0(tid) - 1
                 T nw_v = (idx_next < blockDim.x) ? shared_val[idx_next] : T(-TypeTraits<T>::inf());
                 int nw_pos = (idx_next < blockDim.x) ? shared_pos[idx_next] : -1;
-                warpBitonicMerge<T, N>(v, pos, nw_v, nw_pos); // merge and rebuild in desc order
+                warpBitonicMerge<T, TOP_SIZE>(v, pos, nw_v, nw_pos); // merge and rebuild in desc order
                 shared_val[threadIdx.x] = v;
                 shared_pos[threadIdx.x] = pos;
             }
         }
     }
-    // 至此, 前TOP_SIZE个线程包含了block中最大的TOP_SIZE个元素，但是无序；如果top != 2的幂，则需要排序才能选出确切的topk；
-    // TODO 因此对前TOP_SIZE个线程进行排序是有必要的，需要修改下面排序的逻辑
+    /*  至此, 前TOP_SIZE个线程包含了block中最大的TOP_SIZE个元素，但是无序；如果top != 2的幂，则需要排序才能选出确切的topk；
+        TODO 因此对前TOP_SIZE个线程进行排序是有必要的，需要修改下面排序的逻辑
+        Question: 如果TOP_SIZE =64, 是否可以reduce_end执行到32，然后top64的后32个元素默认已经小于前32个元素了？此时只需要对32~63tid进行一次warpBitonicSort即可实现top64排序？ 
+    */
+
     // intra warp reduce
     if (reduce_end == 32 && threadIdx.x < 32) {
         warpBitonicSort<T, 32>(v, pos, false);
@@ -127,33 +133,6 @@ static __device__ inline void blockBitonicReduce(T& v, int& pos) {
     }
 }
 
-
-template<typename T, int N>
-static __global__ void kernel_bitonic_topk(
-    int n, int top,
-    T *inp,     // (batch, n)
-    float *out,     // (batch, top)
-    int *idx    // (batch, top)
-) {
-    int offset_inp = blockIdx.x * n;
-    int offset_out = blockIdx.x * top;
-    T local_v = threadIdx.x < n ? inp[offset_inp + threadIdx.x] : -TypeTraits<T>::inf();
-    int local_pos = threadIdx.x;
-    warpBitonicSort<T, N>(local_v, local_pos, false); // local sort in desc order
-    for (int i = blockDim.x; i < n; i += blockDim.x) {
-        T nw_v = (i + threadIdx.x) < n ? inp[offset_inp + i + threadIdx.x] : -TypeTraits<T>::inf();
-        int nw_pos = i + threadIdx.x;
-        // step.1: local sort
-        warpBitonicSort<T, N>(nw_v, nw_pos, true); // local sort in asc order
-        // step.2&3: merge and rebuild
-        warpBitonicMerge<T, N>(local_v, local_pos, nw_v, nw_pos); // merge and rebuild in desc order
-    }
-    blockBitonicReduce<T, N>(local_v, local_pos);
-    if (threadIdx.x < top) {
-        out[offset_out + threadIdx.x] = local_v;
-        idx[offset_out + threadIdx.x] = local_pos;
-    }
-}
 // intra-block topk
 // gridDim(batch, n / 1024, 1), threadDim(1024, 1, 1)
 // N = top_size(16/32/64)
@@ -162,8 +141,11 @@ static __global__ void kernel_bitonic_topk_multiblock(
     int n, // dim
     const T *inp,       // (batch, n)
     const int *idx_inp, // (batch, n)
-    T *out,     // (batch, n / 1024 * N)
-    int *idx    // (batch, n / 1024 * N)
+    T *buf_out,     // (batch, n / 1024 * N)
+    int *buf_idx,    // (batch, n / 1024 * N)
+    int top, // top <= top_size
+    T *out,// final_out (batch, top)
+    int *idx// final_idx (batch, top)
 ) {
     int offset_col = blockIdx.y * blockDim.x + threadIdx.x;
     int offset_inp = blockIdx.x * n + offset_col;
@@ -173,23 +155,30 @@ static __global__ void kernel_bitonic_topk_multiblock(
     int local_pos = (idx_inp == nullptr) ? offset_col : ((offset_col < n) ? idx_inp[offset_inp] : -1); // 用来记录数据在最初的输入x中的位置
     
     if (FIRST) warpBitonicSort<T, TOP_SIZE>(local_v, local_pos, false); // local sort in desc order
-    blockBitonicReduce<T, N>(local_v, local_pos);
-    // satge1 : TODO 第二次调用Reduce之后，top_val已经在 tid<=top_size的warp中，但是如果有多个warp暂时是分别降序(warp之间需要再次排序)
+    blockBitonicReduce<T, TOP_SIZE>(local_v, local_pos); 
+    // satge1 : 调用Reduce之后，1、top_val已经在 tid<=top_size的warp中，2、如果有多个warp暂时是分别降序(warp之间需要再次排序)
 
     // stage2: TODO 排序之后就可以按照top的数量写入out pos 了，需要按顺序
-    if (threadIdx.x < N) {
-        out[offset_out] = local_v;
-        idx[offset_out] = local_pos;
+    bool is_over = (CEIL_DIV(n, 1024) * TOP_SIZE) <= TOP_SIZE; // ture:最后一次执行，要写入out pos
+    if (!is_over && threadIdx.x < TOP_SIZE) {
+        buf_out[offset_out] = local_v;
+        buf_idx[offset_out] = local_pos;
+    }
+    if(is_over && threadIdx.x < top) {
+        // 最后一次执行，写入out pos
+        int result_offset = blockIdx.x * top + threadIdx.x; // 定位输出到out的位置
+        out[result_offset] = local_v;
+        idx[result_offset] = local_pos;
     }
 }
 // copy kernel
 // gridDim(batch, 1, 1),   blockDim(top, 1, 1)
 template<typename T>
 static __global__ void kernel_bitonic_topk_multiblock_copy (
-    int n, int top,
+    int n, int top,     // n = top_size, top <= top_size
     const T *inp,       // (batch, n)
     const int *idx_inp, // (batch, n)
-    T *out,         // (batch, top)
+    T *out,             // (batch, top)
     int *idx            // (batch, top)
 ) {
     int offset_inp = blockIdx.x * n + threadIdx.x;
@@ -199,15 +188,40 @@ static __global__ void kernel_bitonic_topk_multiblock_copy (
         idx[offset_out] = idx_inp[offset_inp];
     }
 }
-#define ROUND_UP_POW2(x) ((x) <= 1 ? 1 : (1 << (32 - __clz((x) - 1)))) // round up to the next power of 2
+// #define ROUND_UP_POW2(x) ((x) <= 1 ? 1 : (1 << (32 - __clz((x) - 1)))) // round up to the next power of 2
+// #define TOPK_SIZE_DISPATCH(top, ...) \
+//     do { \
+//         const int &top_v = top; \
+//         const int top_size = ROUND_UP_POW2(top_v); \
+//         __VA_ARGS__ \
+//     } while(0)
+
 #define TOPK_SIZE_DISPATCH(top, ...) \
     do { \
         const int &top_v = top; \
-        const int top_size = ROUND_UP_POW2(top_v); \
-        __VA_ARGS__ \
+        if(top_v > 32){ \
+           const int top_size = 64; \
+            __VA_ARGS__ \
+        }else if (top_v > 16) { \
+            const int top_size = 32; \
+            __VA_ARGS__ \
+        } else if (top_v > 8) { \
+            const int top_size = 16; \
+            __VA_ARGS__ \
+        } else if (top_v > 4) { \
+            const int top_size = 8; \
+            __VA_ARGS__ \
+        } else if (top_v > 2) { \
+            const int top_size = 4; \
+            __VA_ARGS__ \
+        } else if (top_v > 1) { \
+            const int top_size = 2; \
+            __VA_ARGS__ \
+        } else { \
+            const int top_size = 1; \
+            __VA_ARGS__ \
+        } \
     } while(0)
-
-
 template <typename T>
 void bitonic_topk(
     const Stream& stream,
@@ -226,46 +240,40 @@ void bitonic_topk(
         bool first = true;
         dim3 blockDim(1024, 1, 1);
         unsigned int tmp_n = n;// dim
-        std::cout<<"bitonic_topk:179--> tmp_n: " << tmp_n << ", n: " << n << ", top_size: " << top_size << ", batch:" << batch << std::endl;
-        // tmp_n <= 32768 --> 1024 --> top_size
+        // std::cout<<"bitonic_topk:179--> tmp_n: " << tmp_n << ", n: " << n << ", top_size: " << top_size << ", batch:" << batch << std::endl;
         do {
             dim3 gridDim(batch, CEIL_DIV(tmp_n, 1024), 1); // grid_size (batch, 4, 1)
             if (first) {  
-                kernel_bitonic_topk_multiblock<T, top_size, first><<<gridDim, blockDim, 0, stream.stream>>>(
+                kernel_bitonic_topk_multiblock<T, top_size, true><<<gridDim, blockDim, 0, stream.stream>>>(
                     tmp_n,
                     x,
                     nullptr,
-                    buf_val,
-                    buf_pos
+                    buf_val, // num_tokens * CEIL_DIV(dim, 1024) * top_size * sizeof(T)
+                    buf_pos, // num_tokens * CEIL_DIV(dim, 1024) * top_size * sizeof(int)
+                    top,
+                    out,   // num_tokens * top * sizeof(T)
+                    pos // num_tokens * top * sizeof(int)
                 );
                 first = false;
             } else {
-                kernel_bitonic_topk_multiblock<T, top_size, first><<<gridDim, blockDim, 0, stream.stream>>>(
+                kernel_bitonic_topk_multiblock<T, top_size, false><<<gridDim, blockDim, 0, stream.stream>>>(
                     tmp_n,
                     buf_val,
                     buf_pos,
                     nw_buf_val,
-                    nw_buf_pos
+                    nw_buf_pos,
+                    top,
+                    out,
+                    pos
                 );
-                buf_val = nw_buf_val; // may need to optimize
-                buf_pos = nw_buf_pos;
+                // buf_val = nw_buf_val; // may need to optimize
+                // buf_pos = nw_buf_pos;
+                std::swap(buf_val, nw_buf_val);
+                std::swap(buf_pos, nw_buf_pos);
             }
             tmp_n = CEIL_DIV(tmp_n, 1024) * top_size;
         } while (tmp_n > top_size);
 
-        // copy to output tensor
-        // TODO: kernel fusion(把copy操作放进其他kernel中避免启动核函数开销)
-        {
-            dim3 gridDim(batch, 1, 1);
-            blockDim = dim3(top_size, 1, 1);
-            kernel_bitonic_topk_multiblock_copy<T><<<gridDim, blockDim, 0, stream.stream>>>(
-                top_size, top,
-                buf_val,
-                buf_pos,
-                out,
-                pos
-            );
-        }
     });
 }
 
@@ -289,11 +297,10 @@ public:
     int dim, top;
     T* topk_val;
     int* topk_pos;
-    T* tmp_x;
 
     TopK(const int dim, const int top) {
         this->dim = dim; // 4096
-        this->top = top; // 32/64
+        this->top = top; // 16/32/64
     }
     int64_t init_output_ptr(Memory* memory, int32_t num_tokens, int64_t offset) {
         TOPK_SIZE_DISPATCH(top, {
@@ -302,9 +309,6 @@ public:
             offset = memory->allocate((void**)&nw_buf_val, offset, num_tokens * CEIL_DIV(dim, 1024) * top_size * sizeof(T));
             offset = memory->allocate((void**)&nw_buf_pos, offset, num_tokens * CEIL_DIV(dim, 1024) * top_size * sizeof(int));
         });
-        if (top > 32) { // tmp fix
-            offset = memory->allocate((void**)&tmp_x, offset, num_tokens * dim * sizeof(T));
-        }
         offset = memory->allocate((void**)&topk_val, offset, num_tokens * top * sizeof(T));
         offset = memory->allocate((void**)&topk_pos, offset, num_tokens * top * sizeof(int));
         return offset;
@@ -321,7 +325,8 @@ public:
         if (dim == -1) dim = this->dim;
         if (top == -1) top = this->top;
         // int batch, int n, int top, const T *x, T *out, int *pos, T *buf_val, int *buf_pos, T *nw_buf_val, int *nw_buf_pos
-        std::cout<<"Location-->topk.cuh:270--> bitonic_topk: num_tokens: " << num_tokens << ", dim: " << dim << ", top: " << top << std::endl;
+        // std::cout<<"Location-->topk.cuh:270--> bitonic_topk: num_tokens: " << num_tokens << ", dim: " << dim << ", top: " << top << std::endl;
+        // TODO：把大于32的情况考虑进kernel，重复利用第一次bitonic_topk的结果；
         bitonic_topk<T>( 
             stream,
             num_tokens,
@@ -331,23 +336,7 @@ public:
             this->buf_val, this->buf_pos,
             this->nw_buf_val, this->nw_buf_pos
         );
-        // TODO：把大于32的情况考虑进kernel，重复利用第一次bitonic_topk的结果；
-        // 目前这段代码相当于把算好的top_val设负无穷，再循环执行bitonic_topk 性能非常差
-        if (top > 32) {
-            for (int i = 32; i < top; i += 32) {
-                cudaMemcpyAsync(this->tmp_x, input, num_tokens * dim * sizeof(T), cudaMemcpyDeviceToDevice, stream.stream);
-                set_topk_to_neg_inf(stream, num_tokens, dim, top, 32, this->tmp_x, this->topk_pos + (i - 32));
-                bitonic_topk<T>(
-                    stream,
-                    num_tokens,
-                    dim, top,
-                    this->tmp_x,
-                    this->topk_val + i, this->topk_pos + i,
-                    this->buf_val, this->buf_pos,
-                    this->nw_buf_val, this->nw_buf_pos
-                );
-            }
-        }
+
     }
 };
 } // namespace functions
